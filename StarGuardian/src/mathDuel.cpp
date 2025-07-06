8b0ff58821b4b090818409273878527ecce2e587
Carlosscz04 <00062725@uca.edu.sv>
2025-07-05 23:50:20 -0600

    Inicio de la función, encabezado del juego y primera parte de la logica principal del juego MathDuel

--- StarGuardian/src/mathDuel.cpp
+++ StarGuardian/src/mathDuel.cpp
@@ -9,3 +9,57 @@
 using namespace std; 
 
 bool MathDuel()
+{
+    int correctAnswers = 0;
+    
+    setColor(12); // Blue
+cout << "╔══════════════════════════════════════════════════════════════╗" << endl;
+setColor(9); // Yellow (ASCII)
+cout << R"(
+ ____    ____         _   __       ______                    __   
+|_   \  /   _|       / |_[  |     |_   _ `.                 [  |  
+  |   \/   |   ,--. `| |-'| |--.    | | `. \ __   _   .---.  | |  
+  | |\  /| |  `'_\ : | |  | .-. |   | |  | |[  | | | / /__\\ | |  
+ _| |_\/_| |_ // | |,| |, | | | |  _| |_.' / | \_/ |,| \__., | |  
+|_____||_____|\'-;__/\__/[___]|__]|______.'  '.__.'_/ '.__.'[___] 
+)" << endl;
+setColor(12); // Blue
+cout << "╚══════════════════════════════════════════════════════════════╝" << endl;
+setColor(7); // Reset
+   
+    cout<<"You have to solve the following mathematical exercises, also you have 5 seconds..."<<endl;
+    cout << "Solve at least 3 out of 5 to win!" << endl;
+
+    for(int i = 1; i <= 5; i++)
+    {
+         int num1 = rand() % 10 + 1;
+         int num2 = rand() % 10 + 1; 
+         
+         int operation = rand() % 3;
+
+         int result;
+         char symbol;
+
+         switch (operation) {
+            case 0:
+                result = num1 + num2;
+                symbol = '+';
+                break;
+            case 1:
+                result = num1 - num2;
+                symbol = '-';
+                break;
+            case 2:
+                result = num1 * num2;
+                symbol = '*';
+                break;
+        }
+
+        cout << "Question " << i << ": " << num1 << " " << symbol << " " << num2 << " = ";
+        int answer;
+
+        auto start = chrono::steady_clock::now();
+        cin >> answer;
+        auto end = chrono::steady_clock::now();
+
+        chrono::duration<double> elapsed = end - start;
