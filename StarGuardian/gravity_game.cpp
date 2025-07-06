9a638710f48fa135ebcf199ae814668c26a0c0b2
netopalacios <00119525@uca.edu.sv>
2025-07-06 14:52:49 -0600

    Funcion para selecionar un mapa al azar en gravity_game

--- StarGuardian/gravity_game.cpp
+++ StarGuardian/gravity_game.cpp
@@ -4,3 +4,44 @@
 #include <cstring>
 #include "gravity_game.h"
 using namespace std;
+
+void selectRandomMaze() {
+    srand(time(0)); // Random seed
+    int option = rand() % 3 + 1; // Random number between 1 and 3
+
+    switch (option) {
+        case 1: {
+            char map1[maze_size][maze_size] = {
+                {'>', 'v', '>', 'v', '>'},
+                {'^', '>', 'v', '<', 'v'},
+                {'^', '^', '>', 'v', '>'},
+                {'v', '>', '^', '>', 'v'},
+                {'^', '^', '>', '^', '>'}
+            };
+            memcpy(maze, map1, sizeof(map1));
+            break;
+        }
+        case 2: {
+            char map2[maze_size][maze_size] = {
+                {'v', '>', '>', 'v', 'v'},
+                {'v', '^', '<', '^', '>'},
+                {'>', '>', 'v', '<', 'v'},
+                {'^', '^', '>', 'v', '>'},
+                {'^', '>', '^', '>', '>'}
+            };
+            memcpy(maze, map2, sizeof(map2));
+            break;
+        }
+        case 3: {
+            char map3[maze_size][maze_size] = {
+                {'>', '>', 'v', '<', 'v'},
+                {'v', '>', 'v', 'v', '>'},
+                {'^', '^', '>', '>', 'v'},
+                {'^', 'v', '<', 'v', '<'},
+                {'^', '>', '<', '>', '>'}
+            };
+            memcpy(maze, map3, sizeof(map3));
+            break;
+        }
+    }
+}
